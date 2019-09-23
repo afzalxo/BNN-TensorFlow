69c563e65b021a50c96b824ebf2622bec6cbf1f3
Afzal Ahmad <afzalahmad0203@gmail.com>
2019-09-23 17:54:36 +0500

    Training with adaptive learning rate, OOM for now

--- main_mnist.py
+++ main_mnist.py
@@ -11,18 +11,53 @@ def one_hot_labels(labels, dimension=10):
         res[i, seq] = 1
     return res
 
+# A function that shuffles a dataset, credits to https://github.com/uranusx86/BinaryNet-on-tensorflow
+def shuffle(X,y):
+    print(len(X))
+    shuffle_parts = 1
+    chunk_size = int(len(X)/shuffle_parts)
+    shuffled_range = np.arange(chunk_size)
+
+    X_buffer = np.copy(X[0:chunk_size])
+    y_buffer = np.copy(y[0:chunk_size])
+
+    for k in range(shuffle_parts):
+
+        np.random.shuffle(shuffled_range)
+
+        for i in range(chunk_size):
+
+            X_buffer[i] = X[k*chunk_size+shuffled_range[i]]
+            y_buffer[i] = y[k*chunk_size+shuffled_range[i]]
+
+        X[k*chunk_size:(k+1)*chunk_size] = X_buffer
+        y[k*chunk_size:(k+1)*chunk_size] = y_buffer
+
+    return X,y
+
+def train_epoch(inp, y, training, X, lab, sess, train_step, batch_size=100):
+    batches = int(len(X)/batch_size)
+    for i in range(batches):
+        sess.run([train_step], feed_dict={inp:X[i*batch_size:(i+1)*batch_size], y: lab[i*batch_size:(i+1)*batch_size], training:True})
+
+
 def main():
     batch_size = 100
     n_input = 28*28
     n_hidden = 2048
     n_output = 10
-    learning_rate = 3e-3
     drop_in = 0.2
     drop_hidden = 0.5
-    iterations = 1000
+    epochs = 1000
+    learning_rate_start = 3e-3
+    learning_rate_end = 3e-7
+    learning_rate_decay = (learning_rate_end/learning_rate_start)**(1./epochs)
     mnist_data = input_data.read_data_sets("MNIST_data/", one_hot=True)
     inp = tf.placeholder(tf.float32, [None, n_input])
     y = tf.placeholder(tf.float32, [None, n_output])
+    training = tf.placeholder(tf.bool)
+    g_step = tf.Variable(0, trainable=False)
+    learning_rate = tf.train.exponential_decay(learning_rate_start, global_step = g_step, decay_steps = int(mnist_data.train.images.shape[0]/batch_size), decay_rate=learning_rate_decay)
     weights = { #Glorot normal initializer, draws samples from a truncated normal distribution centered at 0 with std dev = sqrt(2/(fan_in + fan_out))
     'w0': tf.Variable(tf.truncated_normal([n_input, n_hidden], stddev=np.sqrt(2/(n_input+n_hidden)))), 
     'w1': tf.Variable(tf.truncated_normal([n_hidden, n_hidden], stddev=np.sqrt(2/(n_hidden+n_hidden)))), 
@@ -36,28 +71,35 @@ def main():
     'b3': tf.Variable(tf.constant(0.1, shape=[n_output]))
     }
     
-    res = binn_mlp_mnist(inp, weights, biases, use_bias = True, training=True)
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = res, labels = y)
+    res = binn_mlp_mnist(inp, weights, biases, use_bias = True, training=training)
+    cross_entropy = tf.square(tf.maximum(0., 1.-y*res))
     loss = tf.reduce_mean(cross_entropy)
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss = cross_entropy)
+    all_trainable_vars = [var for var in tf.trainable_variables()]
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss = loss, var_list=all_trainable_vars, global_step=g_step)
     correct_pred = tf.equal(tf.argmax(res, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     init = tf.global_variables_initializer()
     sess= tf.Session()
     sess.run(init)
-    
-    for ep in range(0, epochs):
-        batch_x, batch_y = mnist_data.train.next_batch(batch_size)#train_x[ep*batch_size:(ep*batch_size+batch_size)], train_y[ep*batch_size:(ep*batch_size+batch_size)]
-        sess.run(train_step, feed_dict={inp:batch_x, y:batch_y})
-        if ep%100 == 0:
-            b_loss, b_acc = sess.run([loss, accuracy], feed_dict={inp:batch_x, y:batch_y})
-            print("Iteration: " +str(ep) + ", Train Loss = " + str(b_loss) + ", Train Acc = " + str(b_acc))
-            test_b_x, test_b_y = mnist_data.test.next_batch(batch_size)
-            test_loss, test_acc = sess.run([loss, accuracy], feed_dict={inp: test_b_x, y:test_b_y})
-            print("Test Loss = " + str(test_loss) + ", Test Acc = " + str(test_acc))
 
+    old_acc = 0.0
+    X_train, y_train = shuffle(mnist_data.train.images, mnist_data.train.labels)
+    for i in range(epochs):
+        train_epoch(inp, y, training, X_train, y_train, sess, train_step, batch_size)
+        X_train, y_train = shuffle(mnist_data.train.images, mnist_data.train.labels)
 
+        hist = sess.run([accuracy, loss],
+                    feed_dict={
+                        inp: mnist_data.test.images,
+                        y: mnist_data.test.labels,
+                        training: False
+                    })
+        print(hist)
+
+        if hist[0] > old_acc:
+            old_acc = hist[0]
+            save_path = saver.save(sess, "./binn_model/model.ckpt")
 
 if __name__ == '__main__':
     main()
