d802f45d8b7f594eb47c45e6d219284d5bc2bee0
Afzal Ahmad <afzalahmad0203@gmail.com>
2019-10-06 11:59:28 +0500

    Added model saving in .npz format for segmentation of training and inference

--- bnn_misc.py
+++ bnn_misc.py
@@ -6,27 +6,42 @@ layer_names = []
 def sign_binarize(inp):
     h_sig = tf.clip_by_value((inp+1.)/2., 0, 1)
     round_out = tf.round(h_sig)
-    round_fin =  h_sig + tf.stop_gradient(round_out - h_sig)
+    round_fin = h_sig + tf.stop_gradient(round_out - h_sig)
     return (2.*round_fin - 1.)
 
 def bin_dense_layer(in_act, num_out, use_bias=True, bin_inp=True, training=True, name='Bin_Dense_L'):
     with tf.variable_scope(name+'_params', reuse=False):
         l_w = tf.get_variable('weight', [in_act.shape[1], num_out], initializer = tf.random_uniform_initializer(-1, 1), constraint=lambda w: tf.clip_by_value(w, -1., 1.), trainable=True)
         l_b = tf.get_variable('bias', [num_out], initializer=tf.zeros_initializer(), trainable=True)
+        #l_mod = tf.get_variable('modulo', [1], initializer=tf.random_uniform_initializer(8000, 10000), constraint=lambda w: w, dtype=tf.float32,trainable=False)
     tf.add_to_collection(name+'_w', l_w)
     layer_names.append(name)
     bin_w = sign_binarize(l_w)
     res = tf.matmul(in_act, bin_w)
-    res = tf.nn.bias_add(res, l_b) if use_bias else res
-    return res
+    res = (tf.nn.bias_add(res, sign_binarize(l_b))) if use_bias else res
+    #l_mod = 999999.
+    #min_val = tf.math.abs(tf.reduce_min(res))
+    #max_val = tf.math.abs(tf.reduce_max(res))+min_val
+    #ones_vec = tf.ones(shape=[None, num_out], dtype=tf.float32)
+    #casted_min = ones_vec * min_val
+    #print(castd_min, res.shape)
+    #casted_min = tf.reshape(min_val,shape=[None, num_out])
+    debug_port0 = l_w
+    debug_port1 = l_b
+    #l_mod = max_val-200
+    #res1 = tf.mod(res+min_val, l_mod)
+    #res = res1 - tf.math.reduce_mean(res1)
+    return res, debug_port0, debug_port1
 
 def compute_gradients(loss, optimizer):
     gradient_list = []
     weight_updates = []
     for l_name in layer_names:
         params = tf.get_collection(l_name + '_w')
+        print("-----> Prams: --------->")
+        print(params)
         if params:
-            grad = optimizer.compute_gradients(loss, params[0])
+            grad = optimizer.compute_gradients(loss, var_list=params[0])
             gradient_list.append(grad[0][0])
             weight_updates.extend(params)
     return zip(gradient_list, weight_updates)
--- main_mnist.py
+++ main_mnist.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import time
+import sys
 import math
 from tensorflow.examples.tutorials.mnist import input_data
 from bnn_mlp import binn_mlp_mnist
 from bnn_misc import compute_gradients
+import matplotlib.pyplot as plt
 
 def one_hot_labels(labels, dimension=10):
     res = np.zeros((labels.shape[0], dimension))
@@ -35,20 +37,20 @@ def shuffle(X,y):
 
     return X,y
 
-def train_epoch(inp, y, training, acc, lo, X, lab, sess, train_kern_step, train_bn_step, batch_size=100):
+def train_epoch(inp, y, training, acc, lo, X, lab, sess, train_bn_step, batch_size=100):
     batches = int(len(X)/batch_size)
     for i in range(batches):
-        hist0 = sess.run([train_kern_step, train_bn_step], feed_dict={inp:X[i*batch_size:(i+1)*batch_size], y: lab[i*batch_size:(i+1)*batch_size], training:True})
-
+        hist0 = sess.run([acc, lo, train_bn_step], feed_dict={inp:X[i*batch_size:(i+1)*batch_size], y: lab[i*batch_size:(i+1)*batch_size], training:True})
+        print(hist0[0], hist0[1])
 def main():
     batch_size = 100
     n_input = 28*28
-    n_hidden = 2048
+    n_hidden = 4096
     n_output = 10
     drop_in = 0.2
     drop_hidden = 0.5
-    epochs = 1000
-    learning_rate_start = 3e-3
+    epochs = 10
+    learning_rate_start = 3e-2
     learning_rate_end = 3e-7
     learning_rate_decay = (learning_rate_end/learning_rate_start)**(1./epochs)
     mnist_data = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -67,20 +69,22 @@ def main():
     g_step_kern = tf.Variable(0, trainable=False)
     g_step_bn = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(learning_rate_start, global_step = g_step_kern, decay_steps = int(mnist_data.train.images.shape[0]/batch_size), decay_rate=learning_rate_decay)
-    res = binn_mlp_mnist(inp, use_bias = True, training=training)
+    res, l0_w, l0_b, l1_w, l1_b, l2_w, l2_b, l3_w, l3_b = binn_mlp_mnist(inp, use_bias = True, training=training)
     cross_entropy = tf.square(tf.maximum(0., 1.-y*res))
     loss = tf.reduce_mean(cross_entropy)
-    all_trainable_vars = [var for var in tf.trainable_variables() if not var.name.endswith('kernel:0')]
+    all_trainable_vars = [var for var in tf.trainable_variables()]
     print("--All Trainable Vars------------------------>>>>>>>")
     print(all_trainable_vars)
     print("--End All Trainable Vars-------------------->>>>>>>")
+    print("--All Global Vars------------------------>>>>>>>")
+    print([var for var in tf.global_variables()])
+    print("--End All Global Vars-------------------->>>>>>>")
     update_operations = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     print("--Update Ops-------------------------------->>>>>>>")
     print(update_operations)
     print("--End Update Ops---------------------------->>>>>>>")
     with tf.control_dependencies(update_operations):
         optimizer = tf.train.AdamOptimizer(learning_rate)
-        train_kernel_step = optimizer.apply_gradients(compute_gradients(loss, optimizer), global_step=g_step_kern)
         train_bn_step = optimizer.minimize(loss = loss, var_list=all_trainable_vars, global_step=g_step_bn)
     correct_pred = tf.equal(tf.argmax(res, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
@@ -95,7 +99,7 @@ def main():
     X_train, y_train = shuffle(mnist_data.train.images, mnist_data.train.labels)
     t_start = time.time()
     for i in range(epochs):
-        train_epoch(inp, y, training, accuracy, loss, X_train, y_train, sess, train_kernel_step, train_bn_step, batch_size)
+        train_epoch(inp, y, training, accuracy, loss, X_train, y_train, sess, train_bn_step, batch_size)
         X_train, y_train = shuffle(mnist_data.train.images, mnist_data.train.labels)
 
         hist = sess.run([accuracy, loss],
@@ -105,12 +109,36 @@ def main():
                         training: False
                     })
         print("Epoch %d, Test Acc: %f, Loss %f, Current Best Acc: %f" % (i, hist[0], hist[1], old_acc))
-
         if hist[0] > old_acc:
+            net_params = sess.run(tf.global_variables())
+            net_params = net_params[2:]
+            np.savez('bnn_mnist_10ep.npz', l0_w=net_params[0], l0_b=net_params[1], l0_gamma=net_params[2], l0_beta=net_params[3], l0_mean=net_params[4], l0_variance=net_params[5], l1_w=net_params[6], l1_b=net_params[7], l1_gamma=net_params[8], l1_beta = net_params[9], l1_mean=net_params[10], l1_variance=net_params[11], l2_w=net_params[12],l2_b=net_params[13], l2_gamma=net_params[14], l2_beta=net_params[15], l2_mean = net_params[16], l2_variance=net_params[17],l3_w=net_params[18],l3_b=net_params[19], l3_gamma=net_params[20], l3_beta=net_params[21], l3_mean = net_params[22], l3_variance=net_params[23])
             old_acc = hist[0]
             store_epoch = i
             save_path = saver.save(sess, "./binn_model/model.ckpt")
     t_end = time.time()
+    np.set_printoptions(edgeitems=500)
+    reto = sess.run(res, feed_dict={inp: mnist_data.test.images[0:2], training:False})
+    print(reto)
+    
+   
+    '''
+    x_axi = np.arange(0, 4096)
+    x_axi2 = np.arange(0, 3)
+    y_axi = tem[0][3]
+    y_axi2 = tem[1]
+    fig = plt.figure()
+    plt.plot(x_axi, y_axi, 'b,', label='Scatter of a+b')
+    plt.xlabel('Vector Index')
+    plt.ylabel('a+b')
+    fig.savefig('figure_apb.png')
+    plt.clf()
+    plt.plot(x_axi2, y_axi2, 'b,', label='Scatter of lmod')
+    plt.xlabel('Vector Index')
+    plt.ylabel('lmod')
+    fig.savefig('figure_lmod.png')
+    print(y_axi2)
+    '''
     print("Completed in %f hours" % ((t_end - t_start)/3600.))
     print("Best Accuarcy: %f, Train Epoch on which achieved best accuracy: %d" % (old_acc, store_epoch))
 
